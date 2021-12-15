a1a282223b6bf4dad06645994c905ad36534b9fd
viane <viane@DESKTOP-4QT6ITQ>
2021-12-15 08:12:49 -0600

    Creacion del metodo insertaHash

--- src/hash/Hash.java
+++ src/hash/Hash.java
@@ -10,5 +10,32 @@ package hash;
  * @author viane
  */
 public class Hash {
+    int dato;
+    String datos,Nombre,sueldo;
+    int estado; //0 = Vacío, 1 = Eliminado, 2 = Ocupado
+
+    static int funcion(int n, int m) {
+        return ((n + 1) % m);
+    }
+    
+    static void insertaHash(Hash[] h, int m, int n, String nombre,String suel) {
+        boolean i = false;
+        int j = funcion(n, m);
+        do {
+            if (h[j].estado == 0 || h[j].estado == 1) {
+                h[j].dato = n;
+                h[j].estado = 2;
+                h[j].datos="\nNumero de trabajador:\t"+n+"\nNombre:\t"+nombre+"\nSalario:\t"+suel;
+                i = true;
+            } else {
+                j++;
+            }
+        } while (j < m && !i);
+        if (i) {
+            javax.swing.JOptionPane.showMessageDialog(null, "¡Elemento insertado con éxito!");
+        } else {
+            javax.swing.JOptionPane.showMessageDialog(null, "¡Tabla llena!");
+        }
+    }
     
 }
